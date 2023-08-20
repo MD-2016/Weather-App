0fdaf2e4995d78d7a4dfa5693ae617d4dde32daa
MD <25043596+MD-2016@users.noreply.github.com>
2023-08-20 19:19:26 -0400

    Deciding on processing user input

--- src/internal/server/searchinput/searchinput.go
+++ src/internal/server/searchinput/searchinput.go
@@ -1,21 +1,31 @@
 package searchinput
 
-import (
-	"html/template"
-	"net/http"
-	"strings"
-	"log"
-)
-
 const WEATHER_API = "https://api.weatherapi.com/v1"
 
+type InputType int
+
+const (
+	Zip InputType = iota
+	UkPostCode
+	CaPostCode
+	AirportCode
+)
+
 type WeatherInput struct {
-	City string
-	Zip int
-	UKpost string
-	CApost string
-	AirportCode string
+	Input string
+	Type  InputType
 }
 
+func ProcessInput(wi WeatherInput) {
+	inputLen := len(wi.Input)
 
-func 
+	if inputLen == 5 {
+		wi.Type = Zip
+	} else if inputLen == 3 {
+
+	}
+}
+
+func EvaluateZipCode(input string) bool {}
+
+func EvaluateAirportCode(input string) bool {}
