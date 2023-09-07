0fae7a76b25d5e68d1cec1e73e2672a00f538990
MD <25043596+MD-2016@users.noreply.github.com>
2023-09-06 20:53:08 -0400

    added and working on FormatWeatherApiCall function

--- src/internal/server/formatinput/formatinput.go
+++ src/internal/server/formatinput/formatinput.go
@@ -1,11 +1,15 @@
 package formatinput
 
+import (
+	"fmt"
+)
+
 const WEATHER_API = "http//api.weatherapi.com/v1"
 
 type WeatherURL struct {
 	formattedURL string
 }
 
-func FormatWeatherApiCall() {
-
+func FormatWeatherApiCall(string input) {
+	apiCall := fmt.Sprintf(WEATHER_API + "")
 }
