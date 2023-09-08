8678e3bb5a56fdae2674527a5f93e0cd1faf7ae3
MD <25043596+MD-2016@users.noreply.github.com>
2023-09-07 20:26:45 -0400

    finished api call setup. Need to make the call and check results next.

--- src/internal/server/formatinput/formatinput.go
+++ src/internal/server/formatinput/formatinput.go
@@ -2,14 +2,25 @@ package formatinput
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-const WEATHER_API = "http//api.weatherapi.com/v1"
+const WEATHER_API = "http//api.weatherapi.com/v1/forecast.json?key="
 
 type WeatherURL struct {
 	formattedURL string
 }
 
-func FormatWeatherApiCall(string input) {
-	apiCall := fmt.Sprintf(WEATHER_API + "")
+func FormatWeatherApiCall(input string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("error loading .env file: %s", err)
+	}
+	apiKey := os.Getenv("WEATHER_API_KEY")
+	apiCall := fmt.Sprintf(WEATHER_API+"%s&q=%s", apiKey, input)
+
+	return apiCall
 }
