012fb9e949cd6404709d95af43531d6f26b55db2
MD <25043596+MD-2016@users.noreply.github.com>
2023-09-11 20:18:24 -0400

    setup the request for the api with the handle function. Need to review return fields of the json result

--- src/internal/server/getweatherresults/getweatherresults.go
+++ src/internal/server/getweatherresults/getweatherresults.go
@@ -1,5 +1,36 @@
 package getweatherresults
 
-func getWeather(apiCall string) {
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type WeatherResults struct {
+}
+
+type FormInput struct {
+	UserInput string
+}
+
+func getWeather(apiCall string) error {
+	tmpl := template.Must(template.ParseFiles("../../../pages/index.html"))
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		resp, err := http.Get(apiCall)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		if err := json.NewDecoder(resp.Body).Decode()
+
+	})
 
 }
