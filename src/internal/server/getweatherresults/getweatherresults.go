a2fe756689f5fdf8a6bb09f58cae224bd0bbf0ea
MD <25043596+MD-2016@users.noreply.github.com>
2023-09-13 20:14:09 -0400

    added more fields for the json return from the api

--- src/internal/server/getweatherresults/getweatherresults.go
+++ src/internal/server/getweatherresults/getweatherresults.go
@@ -7,7 +7,30 @@ import (
 	"net/http"
 )
 
-type WeatherResults struct {
+type Weather struct {
+	Location struct {
+		Name string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC float64 `json:"temp_c"`
+		TempF float64 `json:"temp_f"`
+		Condition struct {
+			Text string `json:"text"`
+		}`json:"condition"`
+	}`json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64 `json:"time_epoch"`
+				TempC float64 `json:"temp_c"`
+				TempF float64 `json:"temp_f"`
+				Condition struct {
+					Text string `json:"text"`
+				}`json:"condition"`
+			}`json:"hour"`
+		}`json:"forecastday"`
+	}`json:"forecast"`
 }
 
 type FormInput struct {
